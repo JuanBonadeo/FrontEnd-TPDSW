c97ee4fda178a456dcbb0b92de3c339bb43c3bf1
JuanBonadeo <juancruzbonadeo04@gmail.com>
2025-09-08 10:52:45 -0300

    refactor: Remove unused font imports and simplify class names in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Movie App",
@@ -28,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg2`}
+        className={` antialiased bg2`}
       >  <div className="min-h-screen  text-white p-4">
-            <div className="max-w-5xl xl:max-w-7xl mx-auto pb-25">
+            <div className="max-w-5xl mx-auto pb-25">
               <AuthProvider>
                 {children}
                 
@@ -41,3 +32,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+
