dae98f41497d71c78ef5e4d012497cc4d4a56f33
gonzacaser <gonzacasermeiro@gmail.com>
2025-09-08 10:22:31 -0300

    fix: Update key prop in MoviesGrid to use unique movie ID for improved rendering

--- src/components/movies/Grid/MoviesGrid.tsx
+++ src/components/movies/Grid/MoviesGrid.tsx
@@ -15,7 +15,7 @@ export const MoviesGrid = ({ movies, title }: Props) => (
     <Title title={title} />
     <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
       {movies.map((movie) => (
-        <MovieCard key={movie.id_movie} id_movie={movie.id_movie} title={movie.title} rating={movie.rating} poster_path={movie.poster_path} release_date={movie.release_date} />
+        <MovieCard key={movie.title} id_movie={movie.id_movie} title={movie.title} rating={movie.rating} poster_path={movie.poster_path} release_date={movie.release_date} />
       ))}
     </div>
   </section>
